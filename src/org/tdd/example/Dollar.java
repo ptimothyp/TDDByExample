807beed3cbbbb31e4a16199e4bd182c11545ca40
Timothy Pakkianathan <ptimothyp@gmail.com>
2013-02-26 01:19:52 +1100

    Initial Commit - Completed till Chapter 10.

---
+++ src/org/tdd/example/Dollar.java
@@ -0,0 +1,11 @@
+package org.tdd.example;
+
+public class Dollar extends Money {
+
+
+	public Dollar(int amount, String currency) {
+		super(amount, currency);
+	}
+
+	
+}
---
+++ src/org/tdd/example/Franc.java
@@ -0,0 +1,8 @@
+package org.tdd.example;
+
+public class Franc extends Money{
+
+	public Franc(int amount, String currency)  {
+		super(amount, currency);
+	}
+}
