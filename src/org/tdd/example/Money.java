0ed1f83529220c9f31cfd12139eaa396af30ec35
Timothy Pakkianathan <ptimothyp@gmail.com>
2013-02-26 01:12:49 +1100

    Initial Commint - Completed till Chapter 10.

---
+++ src/org/tdd/example/Money.java
@@ -0,0 +1,38 @@
+package org.tdd.example;
+
+public class Money {
+	protected int amount;
+	protected String currency;
+	
+	Money(int amount, String currency) {
+		this.amount = amount;
+		this.currency = currency;
+	}
+	
+	public boolean equals(Object object) {
+		Money money = (Money) object;
+		return amount == money.amount
+				&& currency().equals(money.currency()); 
+	}
+	
+	public Money times(int multiplier) {
+		return new Money (amount * multiplier, currency);
+	}
+
+	
+	public static Money dollar(int amount) {
+		return new Dollar(amount, "USD");
+	}
+
+	public static Money franc(int amount) {
+		return new Franc(amount, "CHF");
+	}
+
+	public String currency() {
+		return currency;
+	}
+	
+	public String toString() {
+		return amount + " " + currency;
+	}
+}
